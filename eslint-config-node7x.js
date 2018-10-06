395bd448ad03e167d60c4c958d671447185015bf
stuart_zhang <stuartpage@qq.com>
2018-10-06 23:29:36 +0800

    补充被 丢失的 缩进规则 配置

--- eslint-config-node7x.js
+++ eslint-config-node7x.js
@@ -1,7 +1,17 @@
 'use strict';
-
+const {lineIndent} = require('./utils');
 module.exports = {
   extends: [
     'minxing/eslint-config-node7x.js'
-  ]
+  ],
+  rules: {
+    indent: ['error', lineIndent, {
+      SwitchCase: 0,
+      VariableDeclarator: {
+        var: 2,
+        let: 2,
+        const: 3
+      }
+    }]
+  }
 };
