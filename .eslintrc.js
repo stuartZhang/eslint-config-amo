7b85bbb7e0705631d512e1c0cba7e46d695512d3
stuart_zhang <stuartpage@qq.com>
2018-10-07 11:24:46 +0800

    升级 eslint-config-minxing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,12 @@
 'use strict';
-
+const {lineIndent} = require('./utils');
 module.exports = {
-  'parserOptions': {
-    'ecmaVersion': 5,
-    'sourceType': 'module',
-    'ecmaFeatures': {
-      'globalReturn': true,
-      'impliedStrict': false,
-      'jsx': false,
-      'experimentalObjectRestSpread': false
-    }
-  },
-  'env': {
-    'node': true,
-    'es6': true
-  },
+  'root': true,
   'extends': [
-    'eslint:recommended',
-    'minxing/eslint-config-bestpractice.js',
-    'minxing/eslint-config-errors.js',
-    'minxing/eslint-config-es6.js',
-    'minxing/eslint-config-node.js',
-    'minxing/eslint-config-possibleerrors.js',
-    'minxing/eslint-config-stylistic.js',
-    'minxing/eslint-config-var.js'
+    'minxing/eslint-config-node7x.js'
   ],
   'rules': {
-    'indent': ['error', 2, {
+    'indent': ['error', lineIndent, {
       'SwitchCase': 0,
       'VariableDeclarator': {
         'var': 2,
@@ -34,7 +14,5 @@ module.exports = {
         'const': 3
       }
     }]
-  },
-  'parser': 'babel-eslint',
-  'root': true
+  }
 };
